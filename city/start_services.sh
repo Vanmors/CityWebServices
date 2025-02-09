063e9b942c01a103d08ad9825ff7315ad19d3610
Ilya V. Zagorodnikov <zagorodnikov@protei-lab.ru>
2025-02-09 22:56:51 +0300

    rt

--- city/start_services.sh
+++ city/start_services.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
-# Стартуем PostgreSQL
-service postgresql start
+# Запуск PostgreSQL
+echo "Starting PostgreSQL..."
+/etc/init.d/postgresql start
 
-# Стартуем Payara сервер
+if [ ! -d "/var/lib/postgresql/12/main" ]; then
+  echo "Initializing PostgreSQL database..."
+  su postgres -c "initdb --locale en_US.UTF-8 -D /var/lib/postgresql/12/main"
+fi
+
+sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" /etc/postgresql/12/main/postgresql.conf
+
+# Ожидание, чтобы убедиться, что PostgreSQL работает
+sleep 5
+pg_isready -U postgres
+if [ $? -ne 0 ]; then
+  echo "PostgreSQL did not start correctly"
+  exit 1
+fi
+
+# Запуск Payara
+echo "Starting Payara..."
 /opt/payara/payara5/bin/asadmin start-domain &
 
-# Делаем так, чтобы контейнер не завершался, и сервисы продолжали работать
-tail -f /dev/null
+# Логирование процессов
+echo "All services started. Logging..."
+tail -f /var/log/postgresql/postgresql-*.log /opt/payara/payara5/glassfish/domains/domain1/logs/server.log
