4bbe080ffce6cefdd56997bc3ab059d4fe460fdb
imorikov <imorikov@solanteq.com>
2025-02-18 16:36:18 +0300

    Added soap controller

--- route/RouteCalculationService/src/main/kotlin/com/example/Main.kt
+++ route/RouteCalculationService/src/main/kotlin/com/example/Main.kt
@@ -1,7 +1,15 @@
 package com.example
 
+import com.example.controller.RouteSoapService
 import jakarta.ws.rs.ApplicationPath
 import jakarta.ws.rs.core.Application
+import jakarta.xml.ws.Endpoint
 
 @ApplicationPath("/api")
-open class JaxRsApplication : Application()
+open class JaxRsApplication : Application() {
+
+//    init {
+//        Endpoint.publish("http/????", RouteSoapService::class) // todo(is it really need?)
+//    }
+
+}
